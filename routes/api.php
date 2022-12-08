1d9114778fbec8b2e357f2e3aef286057a5e7951
Ridat Maulana <ridatmaulana28@gmail.com>
2022-12-08 16:42:26 +0700

    integrasi API & CRUD lewat API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,10 @@ use App\Http\Controllers\API\AuthController;
 */
 
 Route::middleware('auth:sanctum')->group(function() {
-
+    Route::get('/book',[BookController::class, 'books']);
+    Route::post('/book/create',[BookController::class, 'create']);
+    Route::post('/book/update/{id}',[BookController::class, 'update']);
+    Route::delete('/book/delete/{id}',[BookController::class, 'delete']);
 });
 
 Route::post('/login',[AuthController::class, 'login']);
